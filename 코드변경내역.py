aad953ee7379c05935a227d5bf29f1569fb7575e
jjuns <jjun2648@gmail.com>
2023-07-25 17:20:42 +0900

    변경 내역

--- 코드변경내역.py
+++ 코드변경내역.py
@@ -1,12 +1,14 @@
 from dash import Dash, html, dcc, Input, Output, callback
 import pandas as pd
 import plotly.express as px
+import scipy.stats as stats
+import plotly.io as po
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/jjun2648/jjuns/main/Indicator18_22.csv')
 
 
 app.layout = html.Div([
@@ -15,7 +17,7 @@ app.layout = html.Div([
         html.Div([
             dcc.Dropdown(
                 df['Indicator Name'].unique(),
-                'Fertility rate, total (births per woman)',
+                '사교육비',
                 id='crossfilter-xaxis-column',
             ),
             dcc.RadioItems(
@@ -30,7 +32,7 @@ app.layout = html.Div([
         html.Div([
             dcc.Dropdown(
                 df['Indicator Name'].unique(),
-                'Life expectancy at birth, total (years)',
+                '특목고진학률',
                 id='crossfilter-yaxis-column'
             ),
             dcc.RadioItems(
@@ -47,7 +49,7 @@ app.layout = html.Div([
     html.Div([
         dcc.Graph(
             id='crossfilter-indicator-scatter',
-            hoverData={'points': [{'customdata': 'Japan'}]}
+            hoverData={'points': [{'customdata': '서울'}]}
         )
     ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
     html.Div([
@@ -56,12 +58,12 @@ app.layout = html.Div([
     ], style={'display': 'inline-block', 'width': '49%'}),
 
     html.Div(dcc.Slider(
-        df['Year'].min(),
-        df['Year'].max(),
+        df['year'].min(),
+        df['year'].max(),
         step=None,
         id='crossfilter-year--slider',
-        value=df['Year'].max(),
-        marks={str(year): str(year) for year in df['Year'].unique()}
+        value=df['year'].max(),
+        marks={str(year): str(year) for year in df['year'].unique()}
     ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
 ])
 
@@ -76,14 +78,28 @@ app.layout = html.Div([
 def update_graph(xaxis_column_name, yaxis_column_name,
                  xaxis_type, yaxis_type,
                  year_value):
-    dff = df[df['Year'] == year_value]
+    dff = df[df['year'] == year_value]
 
-    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
-            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
-            hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name']
+    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['value'],
+                     y=dff[dff['Indicator Name'] == yaxis_column_name]['value'],
+                     hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['지역'],
+                     trendline = 'ols'
             )
+    
+    title = ("statistics: " + str(stats.spearmanr(dff[dff['Indicator Name'] == xaxis_column_name]['value'],
+                           dff[dff['Indicator Name'] == yaxis_column_name]['value'])[0]) 
+             + " p-value: " + str(stats.spearmanr(dff[dff['Indicator Name'] == xaxis_column_name]['value'],
+                           dff[dff['Indicator Name'] == yaxis_column_name]['value'])[1]))
+    
+    fig.add_annotation(x=0, y=0.95, xanchor='left', yanchor='bottom',
+                       xref='paper', yref='paper', showarrow=False, align='left',
+                       font=dict(family="Malgun Gothic", size=14, color="#ffffff"),
+                       bordercolor="#c7c7c7", borderwidth=2,
+                       borderpad=4, bgcolor="#F78181",
+                       opacity=0.7,
+                       text=title)
 
-    fig.update_traces(customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'])
+    fig.update_traces(customdata=dff[dff['Indicator Name'] == yaxis_column_name]['지역'])
 
     fig.update_xaxes(title=xaxis_column_name, type='linear' if xaxis_type == 'Linear' else 'log')
 
@@ -96,7 +112,7 @@ def update_graph(xaxis_column_name, yaxis_column_name,
 
 def create_time_series(dff, axis_type, title):
 
-    fig = px.scatter(dff, x='Year', y='Value')
+    fig = px.scatter(dff, x='year', y='value')
 
     fig.update_traces(mode='lines+markers')
 
@@ -109,7 +125,7 @@ def create_time_series(dff, axis_type, title):
                        text=title)
 
     fig.update_layout(height=225, margin={'l': 20, 'b': 30, 'r': 10, 't': 10})
-
+    
     return fig
 
 
@@ -120,7 +136,7 @@ def create_time_series(dff, axis_type, title):
     Input('crossfilter-xaxis-type', 'value'))
 def update_y_timeseries(hoverData, xaxis_column_name, axis_type):
     country_name = hoverData['points'][0]['customdata']
-    dff = df[df['Country Name'] == country_name]
+    dff = df[df['지역'] == country_name]
     dff = dff[dff['Indicator Name'] == xaxis_column_name]
     title = '<b>{}</b><br>{}'.format(country_name, xaxis_column_name)
     return create_time_series(dff, axis_type, title)
@@ -132,10 +148,10 @@ def update_y_timeseries(hoverData, xaxis_column_name, axis_type):
     Input('crossfilter-yaxis-column', 'value'),
     Input('crossfilter-yaxis-type', 'value'))
 def update_x_timeseries(hoverData, yaxis_column_name, axis_type):
-    dff = df[df['Country Name'] == hoverData['points'][0]['customdata']]
+    dff = df[df['지역'] == hoverData['points'][0]['customdata']]
     dff = dff[dff['Indicator Name'] == yaxis_column_name]
     return create_time_series(dff, axis_type, yaxis_column_name)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
